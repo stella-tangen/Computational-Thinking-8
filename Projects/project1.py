56ed295ad7e2dbe32307b2ac2f3b21eaf041ca41
stella-tangen <stellatangen@seattleacademy.org>
2025-03-25 21:39:19 +0000

    test2

--- Projects/project1.py
+++ Projects/project1.py
@@ -19,5 +19,6 @@ s4 = codesters.Sprite("Starbucks.gif", 100, -100)
 s4.set_size(0.3)
 
 
+
 message1 = codesters.Text ("Stella Tangen", 0, 220) 
 message2 = codesters.Text ("Turning in Project 1", 0, -220)
