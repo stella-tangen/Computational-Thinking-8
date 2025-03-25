ac57e7fb0c2e8c28c4f6b26b4b9d2a0fd2f6e6ea
stella-tangen <stellatangen@seattleacademy.org>
2025-03-25 21:36:55 +0000

    test

---
+++ Projects/project1.py
@@ -0,0 +1,23 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+stage.set_background ("summer")
+q1 = codesters.Square (100,100,200,'white')
+q2 = codesters.Square (-100,100,200,'red')
+q3 = codesters.Square (-100,-100,200,'pink')
+q4 = codesters.Square (100,-100,200,'orange')
+
+s1 = codesters.Sprite("cardinal", 100, 100)
+s2 = codesters.Sprite("dog.gif", -100, 100)
+s2.set_size(0.2)
+s3 = codesters.Sprite("shopping-bags.png", -100, -100)
+s3.set_size(0.5)
+s4 = codesters.Sprite("Starbucks.gif", 100, -100)
+s4.set_size(0.3)
+
+
+message1 = codesters.Text ("Stella Tangen", 0, 220) 
+message2 = codesters.Text ("Turning in Project 1", 0, -220)
