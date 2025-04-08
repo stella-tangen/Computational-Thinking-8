65c739580a94b96b60fecafe924723e4643eabad
stella-tangen <stellatangen@seattleacademy.org>
2025-04-08 16:21:40 +0000

    submit

---
+++ Projects/project3.py
@@ -0,0 +1,27 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.goto(100, 0)
+t.color("pink")
+turtle.Screen() .bgcolor("black")
+
+for i in range(200):
+    t.forward(100 + i)
+    t.left(72 + 1)
+t.speed(10)
+
+#new shape
+import turtle 
+
+t = turtle.Turtle()
+
+t.goto(50, -200)
+t.color("cyan")
+
+for i in range(200):
+    t.forward(100 + i)
+    t.left(60 + 1)
+t.speed(10)
+
+turtle.exitonclick()
